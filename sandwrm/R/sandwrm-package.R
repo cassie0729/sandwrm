b803479e10788fba5b32f3c40576c906b39099ef
Zachary Hancock <zachbhancock@gmail.com>
2023-04-03 16:44:40 -0400

    creating pkg

---
+++ sandwrm/R/sandwrm-package.R
@@ -0,0 +1,16 @@
+#' The 'sandwrm' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name sandwrm-package
+#' @aliases sandwrm
+#' @useDynLib sandwrm, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2023). RStan: the R interface to Stan. R package version 2.21.8. https://mc-stan.org
+#'
+NULL
